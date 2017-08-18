2224f3c819b1fba489ef180e24e4da42ab6b1a22
maguanglu <823358533@qq.com>
2017-08-18 19:32:25 +0800

    修改启动APPcase

--- GitHub_Taoche_App/src/Yixin/Taoche/TestScripts/Startup_Test.java
+++ GitHub_Taoche_App/src/Yixin/Taoche/TestScripts/Startup_Test.java
@@ -22,7 +22,7 @@ public class Startup_Test extends App_BaseCase {
 		Assert.assertTrue(driver.getPageSource().contains("请输入品牌或车型"));
 		Assert.assertTrue(homePage.Location().isEnabled());
 		Assert.assertTrue(homePage.Mine().isEnabled());
-		Assert.assertTrue(homePage.MoreSearch().isEnabled());
+		Assert.assertTrue(homePage.SearchInput().isEnabled());
 		Assert.assertTrue(homePage.Oldcar().isEnabled());
 
 		Log.endTestCase("进入APP首页");
